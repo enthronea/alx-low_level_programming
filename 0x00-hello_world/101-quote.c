69e6e7f9165292901749aef3514f1f1f810bb14d
enthronea <enthronepresh@gmail.com>
2022-12-08 11:40:02 -0800

    t8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
- * Return: Always 0 (Success)
+ * main - program entry point
+ * Return: 1 on success
  */
 int main(void)
 {
-	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that place of art is useful\" - Dora Korpar, 2015-10-19\",59");
 	return (1);
 }
