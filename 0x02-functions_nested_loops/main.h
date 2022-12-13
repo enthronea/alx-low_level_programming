0aacd58ba39146afa3c03fa645eea58dad15211c
enthronea <enthronepresh@gmail.com>
2022-12-13 06:02:15 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,6 @@
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -14,5 +13,6 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
-
+int _isalpha(int c);
+int _islower(int c)
 #endif
