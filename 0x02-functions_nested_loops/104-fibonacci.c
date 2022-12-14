332dad990df189b1b72f331d3a8f2e607d949cc7
enthronea <enthronepresh@gmail.com>
2022-12-14 09:25:40 -0800

    16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,58 +1,40 @@
 #include <stdio.h>>
 /**
- * numLength - return the lenth of string
- * @num : operand number
- * Return: number of digits
- */
-int numLength(int num)
-{
-	int lrngth = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-	return (length);
-}
-/**
- *  *main - prints the first 98 fibonaci sequences
- *  Return: 0
+ * main - fibonacci <3
+ * purpose - no hardcode
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
-	short int i = 1, initial0s;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	while (i <= 98)
+	printf("%lu", bef);
+	for (i = 1; i < 91; i++)
 	{
-		if (f10 > 0)
-			printf("%lu", f10);
-		initial0s = numLength(mx) - 1 - numLength(f1);
-		while (f10 > 0 && initial0s > 0)
-		{
-			printf("%i", 0);
-			initial0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (f1 + f2) % mx;
-		tmpo = f10 + f20 + (f1 + f2) / mx;
-		f1 = f2;
-		f10 = f20;
-		f2 = tmp;
-		f20 = tmpo;
-
-		if (i != 98)
-			printf(", ");
-		else
-			printf("\n");
-		i++;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
+	bef1 = (bef / 1);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
 	return (0);
 }
