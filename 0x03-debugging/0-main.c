921d675390f8353023bb366846c675999d86d62a
enthronea <enthronepresh@gmail.com>
2022-12-14 23:01:28 -0800

    main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+i#include "main.h"
+/**
+ * main - Testfunction for positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test function that prints if interger is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98
+		positive_or_-negative(i);
+	return (0);
+}
