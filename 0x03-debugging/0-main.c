8a3ffd4c6bf3f2fe403484dc071c7e2979959ca4
enthronea <enthronepresh@gmail.com>
2022-12-15 00:30:47 -0800

    main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,9 +5,9 @@ i#include "main.h"
  */
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 return (0);
 }
--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int i;
 
-i = 98
-positive_or_-negative(i);
+i = 0;
+positive_or_negative(i);
 return (0);
 }
