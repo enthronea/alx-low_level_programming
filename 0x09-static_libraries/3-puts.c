b68be08bf70fd4f613bfa3b01eb327cbb3f097ec
enthronea <enthronepresh@gmail.com>
2023-01-06 17:47:28 -0800

    task1

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: string to be printed
+ *
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
